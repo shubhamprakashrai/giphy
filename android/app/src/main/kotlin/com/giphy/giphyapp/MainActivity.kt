29a49c4521d18d7326fa191fbdf9fb028bb756aa
Shubham Rai <raishubham063@gmail.com>
2024-09-17 22:57:28 +0530

    project intial setup

---
+++ android/app/src/main/kotlin/com/giphy/giphyapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.giphy.giphyapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,33 @@
+import 'package:get/get.dart';
+
+import '../../splash_screen.dart';
+import '../modules/login/bindings/login_binding.dart';
+import '../modules/login/views/login_view.dart';
+import '../modules/signup/bindings/signup_binding.dart';
+import '../modules/signup/views/signup_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const initial = Routes.splash;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.splash,
+      page: () => const SplashScreen(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => const LoginView(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.SIGNUP,
+      page: () => const SignupView(),
+      binding: SignupBinding(),
+    ),
+   
+  ];
+}
